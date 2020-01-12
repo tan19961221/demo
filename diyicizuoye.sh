f886d56945351b38b6b29be624d7c9af1f5d029b
tan19961221 <edz@tandongdong.local>
2020-01-12 15:34:05 +0800

    pract

---
+++ diyicizuoye.sh
@@ -0,0 +1,13 @@
+mkdir example
+mv ./ex01.c ./example
+mv ./example/ex01.c ./example/example.c
+mkdir practice
+mv ./ex02.c ./practice
+mv ./practice/ex02.c ./practice/ex02.c
+rm ./ex03.c
+touch ./README.md
+echo' ## example/example.c
+     判断是奇数还是偶数
+
+     ## practice/pactice.c
+     比较两个数大小'  >README.md
